85fffda8150099bfaab3614a46f3140065f4d18a
huycuongngo <cuong.nh@softenmind.com>
2024-07-21 10:54:15 +0700

    new message

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.scss'
+import Layout from './pages/Layout'
+import PrivateRoutes from './routes/PrivateRoutes'
+import HomePage from './pages/movie/HomePage'
+import MovieDetailPage from './pages/movie/MovieDetailPage'
+import Login from './pages/user/Login'
+import Register from './pages/user/Register'
+import DashboardAdmin from './pages/user/DashboardAdmin'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+// import { useEffect } from 'react'
+// import { ACCESS_TOKEN } from './constants'
+// import { jwtDecode } from "jwt-decode";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   const accessToken = localStorage.getItem(ACCESS_TOKEN)
+  //   if (accessToken) {
+  //     const { exp } = jwtDecode(accessToken);
+  //     console.log("exp", exp)
+  //   }
+  // }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<PrivateRoutes element={<HomePage />} />} />
+          <Route path='/movies/:id' element={<PrivateRoutes element={<MovieDetailPage />} />} />
+          <Route path='/admin' element={<PrivateRoutes element={<DashboardAdmin />} />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
